e0dfe0628456c46a05606522890131f2f0a2e40c
chengpeng <impc53802956@gmail.com>
2024-08-18 08:31:03 +0800

    support multiple ports and bind local address

--- server.go
+++ server.go
@@ -1,45 +1,79 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net"
 	"os"
+	"time"
+
+	"encoding/json"
 
 	"github.com/armon/go-socks5"
-	"github.com/caarlos0/env/v6"
 )
 
 type params struct {
-	User            string    `env:"PROXY_USER" envDefault:""`
-	Password        string    `env:"PROXY_PASSWORD" envDefault:""`
-	Port            string    `env:"PROXY_PORT" envDefault:"1080"`
-	AllowedDestFqdn string    `env:"ALLOWED_DEST_FQDN" envDefault:""`
-	AllowedIPs      []string  `env:"ALLOWED_IPS" envSeparator:"," envDefault:""`
+	// User      string `json:"user"`
+	// Password  string `json:"password"`
+	Port      int    `json:"port"`
+	LocalAddr string `json:"local_addr"`
 }
 
 func main() {
-	// Working with app params
-	cfg := params{}
-	err := env.Parse(&cfg)
-	if err != nil {
-		log.Printf("%+v\n", err)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: socks5-server <config.json>")
 	}
+	arg := os.Args[1]
+
+	file, err := os.Open(arg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	decoder := json.NewDecoder(file)
+
+	var cfgs []params = make([]params, 0)
+	err = decoder.Decode(&cfgs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, cfg := range cfgs {
+		go runServer(cfg)
+	}
+
+	for {
+		time.Sleep(60 * time.Second)
+	}
+
+}
+
+func runServer(cfg params) {
 
 	//Initialize socks5 config
 	socks5conf := &socks5.Config{
 		Logger: log.New(os.Stdout, "", log.LstdFlags),
 	}
 
-	if cfg.User+cfg.Password != "" {
-		creds := socks5.StaticCredentials{
-			os.Getenv("PROXY_USER"): os.Getenv("PROXY_PASSWORD"),
-		}
-		cator := socks5.UserPassAuthenticator{Credentials: creds}
-		socks5conf.AuthMethods = []socks5.Authenticator{cator}
-	}
+	// if cfg.User+cfg.Password != "" {
+	//     creds := socks5.StaticCredentials{
+	//         os.Getenv("PROXY_USER"): os.Getenv("PROXY_PASSWORD"),
+	//     }
+	//     cator := socks5.UserPassAuthenticator{Credentials: creds}
+	//     socks5conf.AuthMethods = []socks5.Authenticator{cator}
+	// }
 
-	if cfg.AllowedDestFqdn != "" {
-		socks5conf.Rules = PermitDestAddrPattern(cfg.AllowedDestFqdn)
+	socks5conf.Dial = func(ctx context.Context, network, addr string) (net.Conn, error) {
+		dialer := &net.Dialer{
+			Timeout: 10 * time.Second,
+			LocalAddr: &net.TCPAddr{
+				IP:   net.ParseIP(cfg.LocalAddr),
+				Port: 0,
+			},
+		}
+		return dialer.Dial(network, addr)
 	}
 
 	server, err := socks5.New(socks5conf)
@@ -47,17 +81,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Set IP whitelist
-	if len(cfg.AllowedIPs) > 0 {
-		whitelist := make([]net.IP, len(cfg.AllowedIPs))
-		for i, ip := range cfg.AllowedIPs {
-			whitelist[i] = net.ParseIP(ip)
-		}
-		server.SetIPWhitelist(whitelist)
-	}
-
-	log.Printf("Start listening proxy service on port %s\n", cfg.Port)
-	if err := server.ListenAndServe("tcp", ":"+cfg.Port); err != nil {
+	log.Printf("Start listening proxy service on port %d\n", cfg.Port)
+	if err := server.ListenAndServe("tcp", fmt.Sprintf(":%d", cfg.Port)); err != nil {
 		log.Fatal(err)
 	}
 }
